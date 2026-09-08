5f8b8976a80bbf2a2f7018817f131ae235b3c28d
Anais <anais.planas.91@gmail.com>
2026-09-08 18:39:09 +0200

    :ScrollTop para cambio de rutas

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Header, Footer, ScrollToTopButton } from "../components";
+import { Header, Footer, ScrollToTopButton, ScrollTop } from "../components";
 
 export const MainLayout = () => {
   return (
     <>
       <div className="min-h-screen flex flex-col">
+        <ScrollTop />
         <Header />
         <main className="flex-1 flex flex-col">
           <Outlet />
