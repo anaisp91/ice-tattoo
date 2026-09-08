5876eddfa490073780f36d63e1dce8fcec224a7e
Anais <anais.planas.91@gmail.com>
2026-09-08 17:36:26 +0200

    :logo cambiado

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <Link to={"/"}>
       <img
-        src="/images/header/logo-negro.png"
+        src="/images/header/logo-nuevo-negro.png"
         alt="ice tattoo logo"
         className="max-w-[100px]"
       />
